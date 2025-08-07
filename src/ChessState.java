247d3a2615cf659cd7e4795077e78582f196c8b3
Aiden Egglin <uefadude@gmail.com>
2025-08-06 23:51:09 -0400

    Add bitboards

--- src/ChessState.java
+++ src/ChessState.java
@@ -1,31 +1,43 @@
+import java.util.Collections;
+
 public class ChessState
 {
     public static final int BOARD_DIMENSION = 8;
-    public String[][] board;
+    private long whitePawns = 0L, whiteKnights = 0L, whiteBishops = 0L, whiteQueen = 0L, whiteKing = 0L;
+    private long blackPawns = 0L, blackKnights = 0L, blackBishops = 0L, blackQueen = 0L, blackKing = 0L;
 
     private void initializeBoard()
     {
-        board = new String[][]{
-                {"r", "n", "b", "q", "k", "b", "n", "r"},
-                {"p", "p", "p", "p", "p", "p", "p", "p"},
-                {" ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " "},
-                {"P", "P", "P", "P", "P", "P", "P", "P"},
-                {"R", "N", "B", "Q", "K", "B", "N", "R"}
-        };
+        // Can also do String.repeat() for newer versions of Java but this works for Java 8
+        String whitePawnsStr = String.join("", Collections.nCopies(6 * BOARD_DIMENSION, "0")) + "11111111" + String.join("", Collections.nCopies(BOARD_DIMENSION, "0"));
+        String whiteKnightsStr = String.join("", Collections.nCopies((7 * BOARD_DIMENSION) + 1, "0")) + "1" + "000010";
+        String whiteBishopStr = String.join("", Collections.nCopies((7 * BOARD_DIMENSION) + 2, "0")) + "1" + "00100";
+        String whiteQueenStr = String.join("", Collections.nCopies((7 * BOARD_DIMENSION) + 3, "0")) + 1 + "0000";
+        String whiteKingStr = String.join("", Collections.nCopies((7 * BOARD_DIMENSION) + 4, "0")) + "000";
+
+        String blackPawnsStr = String.join("", Collections.nCopies(BOARD_DIMENSION, "0")) + "11111111" + String.join("", Collections.nCopies(BOARD_DIMENSION * 6, "0"));
+        String blackKnightsStr = "01000010" + String.join("", Collections.nCopies(BOARD_DIMENSION * 7, "0"));
+        String blackBishopStr = "00100100" + String.join("", Collections.nCopies(BOARD_DIMENSION * 7, "0"));
+        String blackQueenStr = "00010000" + String.join("", Collections.nCopies(BOARD_DIMENSION * 7, "0"));
+        String blackKingStr = "00001000" + String.join("", Collections.nCopies(BOARD_DIMENSION * 7, "0"));
+        
+        whitePawns = Long.parseLong(whitePawnsStr, 2);
+        whiteKnights = Long.parseLong(whiteKnightsStr, 2);
+        whiteBishops = Long.parseLong(whiteBishopStr, 2);
+        whiteQueen = Long.parseLong(whiteQueenStr, 2);
+        whiteKing = Long.parseLong(whiteKingStr, 2);
+
+        blackPawns = Long.parseLong(blackPawnsStr, 2);
+        blackKnights = Long.parseLong(blackKnightsStr, 2);
+        blackBishops = Long.parseLong(blackBishopStr, 2);
+        blackQueen = Long.parseLong(blackQueenStr, 2);
+        blackKing = Long.parseLong(blackKingStr, 2);
     }
 
     public ChessState()
     {
         initializeBoard();
-    }
-    public String getPossibleMoves()
-    {
-        for (int i = 0; i < BOARD_DIMENSION * BOARD_DIMENSION; i++)
-        {
 
-        }
     }
+
 }
